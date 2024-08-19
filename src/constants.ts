a5f2d5a3235318ced3a8615edb70e72a7a1112dd
nightness <joshguyette@yahoo.com>
2024-08-19 01:58:18 -0500

    renamed example to dev-env, going to build the project from the same source the "example" project is using

--- src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const DEFAULT_ANIMATION_DURATION = 300;
-export const DEFAULT_MAX_DROP_HEIGHT = 225;
-export const DEFAULT_DROPDOWN_BORDER = { color: 'transparent', width: 0, style: 'none', radius: 0 };
-export const DEFAULT_PADDING = 10;
